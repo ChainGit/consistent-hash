99cb9d9987e825072224ce1cdd31da9d60e74b5e
bangquan.qian <chainz@foxmail.com>
2018-11-09 17:16:12 +0800

    整理项目

---
+++ src/main/java/com/chain/consistenthash/consts/ConsistentHashConstants.java
@@ -0,0 +1,9 @@
+package com.chain.consistenthash.consts;
+
+/**
+ * @Author bangquan.qian
+ * @Date 2018/11/9 3:37 PM
+ */
+
+public class ConsistentHashConstants {
+}
---
+++ src/main/java/com/chain/consistenthash/controller/ConsistentHashController.java
@@ -0,0 +1,17 @@
+package com.chain.consistenthash.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author bangquan.qian
+ * @Date 2018/11/9 3:52 PM
+ */
+@Log4j2
+@Controller("/api/consistent-hash/")
+public class ConsistentHashController {
+
+    static {
+        log.error("error");
+    }
+}
